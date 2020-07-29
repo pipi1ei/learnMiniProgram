35c96c60e44f84fe7206b431ab1ab7f6d9540326
leichen28 <leichen28@iflytek.com>
2020-07-29 08:39:31 +0800

    系统API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,39 @@
 //index.js
+
+import request from '../../service/network.js'
+
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    motto: 'Hello World'
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success: res => {
+    //     console.log(res)
+    //   }
+    // }) 
+
+    request({
+      url: 'http://123.207.32.32:8000/recommend',
+    }).then(res => {
+      console.log(res.statusCode)
     })
+  },
+  showActionSheet(){
+    wx.showActionSheet({
+      itemList: ["相册", "拍照"],
+      success: res => {
+        console.log(res)
+      }
+    })
+  },
+  onShareAppMessage(){
+    return {
+      title: '你好啊，李银河'
+    }
   }
 })
